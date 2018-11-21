fbca3c1b23f9a9fdf2e7cac8dd16e917f7a1bb3c
Tim Showers <showerst@gmail.com>
2018-11-21 15:25:00 -0500

    GA 2019

--- openstates/ga/__init__.py
+++ openstates/ga/__init__.py
@@ -50,6 +50,13 @@ class Georgia(Jurisdiction):
             "name": "2018 Special Session",
             'start_date': '2018-11-13',
         },
+        {
+            "_scraped_name": "2019-2020 Regular Session",
+            "identifier": "2019_20",
+            "name": "2019-2020 Regular Session",
+            'start_date': '2019-01-14',
+            'end_date': '2020-03-31'
+        },
     ]
     ignored_scraped_sessions = [
         "2009-2010 Regular Session",
