65eb648ca7fb9cff4cf0ce9a4ff32fe9d80dcb0d
Tim Showers <showerst@gmail.com>
2020-01-30 10:47:40 -0500

    OR: Add 2020 session and blacked files

--- openstates/or/utils.py
+++ openstates/or/utils.py
@@ -6,11 +6,13 @@ def index_legislators(scraper, session_key):
     Get the full name of legislators. The membership API only returns a "LegislatorCode".
     This will cross-reference the name.
     """
-    legislators_response = scraper.api_client.get('legislators', session=session_key)
+    legislators_response = scraper.api_client.get("legislators", session=session_key)
 
     legislators = {}
     for leg in legislators_response:
-        legislators[leg['LegislatorCode']] = '{} {}'.format(leg['FirstName'], leg['LastName'])
+        legislators[leg["LegislatorCode"]] = "{} {}".format(
+            leg["FirstName"], leg["LastName"]
+        )
 
     return legislators
 
@@ -24,4 +26,5 @@ SESSION_KEYS = {
     "2018 Regular Session": "2018R1",
     "2018 Special Session": "2018S1",
     "2019 Regular Session": "2019R1",
+    "2020 Regular Session": "2020R1",
 }
