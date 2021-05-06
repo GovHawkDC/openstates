c7c34ed9095a25f6a3e6a4dc60d5989c86113307
Tim Showers <showerst@gmail.com>
2021-05-06 14:18:53 -0400

    Put NM back

--- scrapers/nm/__init__.py
+++ scrapers/nm/__init__.py
@@ -138,21 +138,21 @@ class NewMexico(State):
             "end_date": "2020-11-27",
         },
         {
-            "_scraped_name": "2021 Regular",
-            "classification": "primary",
-            "identifier": "2021",
-            "name": "2021 Regular Session",
-            "start_date": "2021-01-19",
-            "end_date": "2021-03-20",
-        },
-        {
             "_scraped_name": "2021 Special",
             "classification": "special",
             "identifier": "2021S",
             "name": "2021 Special Session",
             "start_date": "2021-03-30",
             "end_date": "2021-03-31",
-        },        
+        },
+        {
+            "_scraped_name": "2021 Regular",
+            "classification": "primary",
+            "identifier": "2021",
+            "name": "2021 Regular Session",
+            "start_date": "2021-01-19",
+            "end_date": "2021-03-20",
+        },                
     ]
     ignored_scraped_sessions = [
         "2010 2nd Special",
