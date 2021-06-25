4b3209155e98903f5f77955658f100566131f499
Tim Showers <showerst@gmail.com>
2021-06-25 10:35:04 -0400

    CT: catch senate passage

--- scrapers/ct/bills.py
+++ scrapers/ct/bills.py
@@ -266,8 +266,8 @@ class CTBillScraper(Scraper):
                         " AND Office of Fiscal Analysis %s" % (match.group(1))
                     )
 
-                if re.match(r"^ADOPTED, (HOUSE|SENATE)", action) or re.match(
-                    r"^(HOUSE|SENATE) PASSED", action
+                if re.match(r"^ADOPTED, (HOUSE|SENATE|SEN\.?)", action) or re.match(
+                    r"^(HOUSE|SENATE|SEN\.?) PASSED", action
                 ):
                     act_type.append("passage")
 
@@ -277,7 +277,6 @@ class CTBillScraper(Scraper):
 
                 if re.match(r"SIGNED BY GOVERNOR", action):
                     act_type.append("executive-signature")
-
 
                 if re.match(r"^LINE ITEM VETOED", action):
                     act_type.append("executive-veto-line-item")
