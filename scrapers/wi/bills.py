6bf0d53c91e04996f5b56da024f0bc09ac4455aa
Tim Showers <showerst@gmail.com>
2021-08-19 16:17:02 -0400

    WI HACKS

--- scrapers/wi/bills.py
+++ scrapers/wi/bills.py
@@ -56,7 +56,7 @@ class WIBillScraper(Scraper):
         # that we use to scrape the data
 
         while last_url != next_url:
-            html = self.get(next_url).text
+            html = self.get(next_url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).text
             doc = lxml.html.fromstring(html)
             doc.make_links_absolute(next_url)
 
@@ -130,7 +130,7 @@ class WIBillScraper(Scraper):
             bill_type = "bill"
 
         try:
-            data = self.get(url).text
+            data = self.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).text
         except scrapelib.HTTPError:
             self.warning("skipping URL %s" % url)
             return
@@ -162,7 +162,7 @@ class WIBillScraper(Scraper):
 
     def scrape_bill_history(self, bill, url, chamber):
         seen_votes = set()
-        body = self.get(url).text
+        body = self.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).text
         doc = lxml.html.fromstring(body)
         doc.make_links_absolute(url)
 
@@ -204,7 +204,7 @@ class WIBillScraper(Scraper):
                 "Fiscal Estimates and Reports" "Record of Committee Proceedings",
             ):
                 extra_doc_url = a.get("href")
-                extra_doc = lxml.html.fromstring(self.get(extra_doc_url).text)
+                extra_doc = lxml.html.fromstring(self.get(extra_doc_url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).text)
                 extra_doc.make_links_absolute(extra_doc_url)
                 for extra_a in extra_doc.xpath('//ul[@class="docLinks"]/li//a'):
                     if extra_a.text:
@@ -367,7 +367,7 @@ class WIBillScraper(Scraper):
 
     def add_senate_votes(self, vote, url):
         try:
-            html = self.get(url).text
+            html = self.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).text
         except scrapelib.HTTPError:
             self.warning("No Senate Votes found for %s" % url)
             return
@@ -406,7 +406,7 @@ class WIBillScraper(Scraper):
 
     def add_house_votes(self, vote, url):
         try:
-            html = self.get(url).content
+            html = self.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}).content
         except scrapelib.HTTPError:
             self.warning("No House Votes found for %s" % url)
             return
