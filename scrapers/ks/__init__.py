cdea1908bdf830b46a9fe5e564c7eb50cf1faef9
Tim Showers <showerst@gmail.com>
2021-12-16 13:30:47 -0500

    Restore 2022 to default

--- scrapers/ks/__init__.py
+++ scrapers/ks/__init__.py
@@ -70,6 +70,14 @@ class Kansas(State):
             "end_date": "2020-06-05",
         },
         {
+            "_scraped_name": "b2021s",
+            "classification": "special",
+            "identifier": "2021S1",
+            "name": "2021 Special Session",
+            "start_date": "2021-11-22",
+            "end_date": "2021-11-25",
+        },
+        {
             "_scraped_name": "b2021_22",
             "classification": "primary",
             "identifier": "2021-2022",
@@ -77,14 +85,6 @@ class Kansas(State):
             "start_date": "2021-01-11",
             # TODO: set real end date
             "end_date": "2022-05-31",
-        },
-        {
-            "_scraped_name": "b2021s",
-            "classification": "special",
-            "identifier": "2021S1",
-            "name": "2021 Special Session",
-            "start_date": "2021-11-22",
-            "end_date": "2021-11-25",
         },
     ]
     ignored_scraped_sessions = []
