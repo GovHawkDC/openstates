1ed2ed29b4c07744ca18811cd5924815e4942396
Tim Showers <showerst@gmail.com>
2022-08-10 13:44:44 -0400

    Add legistar api bill scraper and ann arbor

---
+++ scrapers/mi-ann/__init__.py
@@ -0,0 +1,69 @@
+import re
+import uuid
+from utils import url_xpath
+from datetime import date
+from openstates.scrape import State
+from openstates.metadata.models import (
+    State as modelState,
+    Chamber,
+    simple_numbered_districts,
+)
+
+from .bills import A2BillScraper
+
+# from .events import A2EventScraper
+
+class AnnArbor(State):
+    division_id = "ocd-division/country:us/state:mi/place:ann_arbor"
+    classification = "legislature"
+    name = "Ann Arbor"
+    legislature_name = "City Council"
+    url = "https://a2gov.legistar.com/"
+    scrapers = {
+        "bills": A2BillScraper,
+        # "events": A2EventScraper,
+    }
+
+    org_id = uuid.uuid4()
+    org_id = f"ocd-organization/{org_id}"
+    metadata = modelState(
+        name="Ann Arbor",
+        abbr="mia2",
+        capital="None",
+        capital_tz="America/Chicago",
+        fips="48",
+        unicameral=True,
+        legislature_name="Ann Arbor City Council",
+        legislature_organization_id="ocd-organization/8ab77a54-0646-413c-a63a-dc85154282b7",
+        executive_name="Executive",
+        executive_organization_id="ocd-organization/4c8c2a9c-f33c-476d-bf81-266eb72193f0",
+        division_id="ocd-division/country:us/state:mi/place:ann_arbor",
+        jurisdiction_id="ocd-division/country:us/state:mi/place:ann_arbor/government",
+        url="https://www.daytonohio.gov/AgendaCenter/City-Commission-2",
+        legislature=Chamber(
+            chamber_type="upper",
+            name="City Council",
+            organization_id=org_id,
+            num_seats=31,
+            title="Councilmember",
+            districts=[]
+            # districts=simple_numbered_districts(
+            #     "ocd-division/country:us/state:tx", "upper", 31
+            # ),
+        ),
+    )
+    legislative_sessions = [
+        {
+            "_scraped_name": "2022",
+            "classification": "primary",
+            "identifier": "2022",
+            "name": "2022 Session",
+            "start_date": "2022-01-01",
+            "end_date": "2022-12-31",
+            "active": True,
+        },
+    ]
+    ignored_scraped_sessions = []
+
+    def get_session_list(self):
+        return [str(date.today().year)]
---
+++ scrapers/mi-ann/bills.py
@@ -0,0 +1,42 @@
+import json
+import lxml
+
+import pytz
+
+from openstates.scrape import Scraper, Event
+
+import datetime as dt
+from utils.ghlegistarapi import GHLegistarAPIBillScraper
+
+
+class A2BillScraper(GHLegistarAPIBillScraper, Scraper):
+    TIMEZONE = "US/Eastern"
+    BASE_URL = "http://webapi.legistar.com/v1/a2gov"
+    BASE_WEB_URL = "https://a2gov.legistar.com/"
+
+    VOTE_OPTIONS = {'yea' : 'yes',
+                    'rising vote' : 'yes',
+                    'nay' : 'no',
+                    'recused' : 'excused',
+                    'yes': 'yes',
+                    'no': 'no',
+                    'excused': 'excused',
+                    'abstain': 'abstain',
+                    'aye': 'yes',
+                    'present': 'present',
+                    'absent': 'absent',
+                    }
+
+    BILL_TYPES = {'Ordinance' : 'ordinance',
+                'Resolution' : 'resolution',
+                'Order' : 'order',
+                'Claim' : 'claim',
+                'Oath of Office' : None,
+                'Communication' : None,
+                'Appointment' : 'appointment',
+                'Action Item': None,
+                'Report' : None,
+                'Ceremonial Item': None,
+                'Minutes': None,
+                'Board/Commission': None,
+                }
