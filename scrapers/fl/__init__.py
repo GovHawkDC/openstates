b75b97293a190185b74792f2b58e8dbfa008454e
Tim Showers <showerst@gmail.com>
2022-12-12 09:05:07 -0500

    FL: 2022A

--- scrapers/fl/__init__.py
+++ scrapers/fl/__init__.py
@@ -135,7 +135,7 @@ class Florida(State):
             "identifier": "2021",
             "classification": "primary",
             "start_date": "2021-03-02",
-            "end_date": "2021-05-01",
+            "end_date": "2021-04-30",
             "active": False,
         },
         {
@@ -143,7 +143,7 @@ class Florida(State):
             "identifier": "2021A",
             "classification": "primary",
             "start_date": "2021-05-12",
-            "end_date": "2021-05-21",
+            "end_date": "2021-05-17",
         },
         {
             "name": "2021 Special Session B",
@@ -158,7 +158,7 @@ class Florida(State):
             "identifier": "2022",
             "classification": "primary",
             "start_date": "2022-01-11",
-            "end_date": "2022-03-11",
+            "end_date": "2022-03-14",
             "active": False,
         },
         {
@@ -174,7 +174,7 @@ class Florida(State):
             "identifier": "2022D",
             "classification": "special",
             "start_date": "2022-05-23",
-            "end_date": "2022-05-30",
+            "end_date": "2022-05-27",
             "active": False,
         },
         {
@@ -185,10 +185,19 @@ class Florida(State):
             "end_date": "2023-05-05",
             "active": True,
         },
+        # Yes, 2022 Special Session A
+        # occurred after B,C, and D
+        {
+            "name": "2022 Special Session A",
+            "identifier": "2022A",
+            "classification": "special",
+            "start_date": "2022-12-12",
+            "end_date": "2022-12-16",
+            "active": True,
+        },
     ]
     ignored_scraped_sessions = [
         *(str(each) for each in range(1997, 2010)),
-        "2022A",
         "2022 Org.",
         "2020 Org.",
         "2019 I",  # Empty, maybe informational session
