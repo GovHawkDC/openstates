046ded60e3bb3065a3fec382a2622448923b4704
Tim Showers <showerst@gmail.com>
2023-07-25 12:16:13 -0400

    USA: Events: Work with new congress.gov api

--- scrapers/usa/events.py
+++ scrapers/usa/events.py
@@ -1,11 +1,16 @@
+import json
 import pytz
 import lxml
 import datetime
 import dateutil
+import os
 import re
 import requests
+import pprint
 
+from requests.models import PreparedRequest
 from utils import LXMLMixin
+from utils.media import get_media_type
 from utils.events import match_coordinates
 from openstates.scrape import Scraper, Event
 from openstates.exceptions import EmptyScrape, ScrapeValueError
@@ -14,6 +19,9 @@ from openstates.exceptions import EmptyScrape, ScrapeValueError
 class USEventScraper(Scraper, LXMLMixin):
     _TZ = pytz.timezone("America/New_York")
     s = requests.Session()
+    api_key = ""
+    chambers = {"upper": "senate", "lower": "house"}
+    event_count = 0
 
     media_types = {
         "PDF": "application/pdf",
@@ -31,12 +39,17 @@ class USEventScraper(Scraper, LXMLMixin):
     buildings = {
         "RSOB": "Russell Senate Office Building, 2 Constitution Ave NE, Washington, DC 20002",
         "SR": "Russell Senate Office Building, 2 Constitution Ave NE, Washington, DC 20002",
+        "Russell Senate Office Building": "Russell Senate Office Building, 2 Constitution Ave NE, Washington, DC 20002",
         "DSOB": "Dirksen Senate Office Building, 100 Constitution Ave NE, Washington, DC 20002",
         "SD": "Dirksen Senate Office Building, 100 Constitution Ave NE, Washington, DC 20002",
+        "Dirksen Senate Office Building": "Dirksen Senate Office Building, 100 Constitution Ave NE, Washington, DC 20002",
         "HSOB": "Hart Senate Office Building, 150 Constitution Ave NE, Washington, DC 20510",
         "SH": "Hart Senate Office Building, 150 Constitution Ave NE, Washington, DC 20510",
+        "Hart Senate Office Building": "Hart Senate Office Building, 150 Constitution Ave NE, Washington, DC 20510",
         "CHOB": "Cannon House Office Building, 27 Independence Ave SE, Washington, DC 20515",
+        "Cannon House Office Building": "Cannon House Office Building, 27 Independence Ave SE, Washington, DC 20515",
         "LHOB": "Longworth House Office Building, 15 Independence Avenue SW, Washington, DC 20515",
+        "Longworth House Office Building": "Longworth House Office Building, 15 Independence Avenue SW, Washington, DC 20515",
         "RHOB": "Rayburn House Office Building, 50 Independence Avenue SW, Washington, DC 20515",
         "FHOB": "Ford House Office Building, 441 2nd Street SW, Washington, D.C. 20515",
         "CAPITOL": "US Capitol, 25 Independence Ave SE, Washington, DC 20004",
@@ -57,41 +70,184 @@ class USEventScraper(Scraper, LXMLMixin):
     # date_filter argument can give you just one day;
     # format is "2/28/2019" per AK's site
     def scrape(self, chamber=None, session=None, date_filter=None):
-        event_count = 0
-        events = set()
+        self.api_key = os.environ["CONGRESS_DOTGOV_API_KEY"]
         if chamber is None:
-            for event in self.scrape_house():
-                if event.dedupe_key in events:
-                    self.warning(f"Duplicate event {event.dedupe_key}")
-                    continue
-                events.add(event.dedupe_key)
-                event_count += 1
-                yield event
-            for event in self.scrape_senate():
-                if event.dedupe_key in events:
-                    self.warning(f"Duplicate event {event.dedupe_key}")
-                    continue
-                events.add(event.dedupe_key)
-                event_count += 1
-                yield event
+            for ch in ["upper", "lower"]:
+                yield from self.scrape_chamber(ch, "118")
+            # for event in self.scrape_house():
+            #     if event.dedupe_key in events:
+            #         self.warning(f"Duplicate event {event.dedupe_key}")
+            #         continue
+            #     events.add(event.dedupe_key)
+            #     event_count += 1
+            #     yield event
+            # for event in self.scrape_senate():
+            #     if event.dedupe_key in events:
+            #         self.warning(f"Duplicate event {event.dedupe_key}")
+            #         continue
+            #     events.add(event.dedupe_key)
+            #     event_count += 1
+            #     yield event
         elif chamber == "lower":
-            for event in self.scrape_house():
-                if event.dedupe_key in events:
-                    self.warning(f"Duplicate event {event.dedupe_key}")
-                    continue
-                events.add(event.dedupe_key)
-                event_count += 1
-                yield event
+            yield from self.scrape_chamber("lower", "118")
+            # for event in self.scrape_house():
+            #     if event.dedupe_key in events:
+            #         self.warning(f"Duplicate event {event.dedupe_key}")
+            #         continue
+            #     events.add(event.dedupe_key)
+            #     event_count += 1
+            #     yield event
         elif chamber == "upper":
-            for event in self.scrape_senate():
-                if event.dedupe_key in events:
-                    self.warning(f"Duplicate event {event.dedupe_key}")
-                    continue
-                events.add(event.dedupe_key)
-                event_count += 1
-                yield event
-        if event_count < 1:
+            yield from self.scrape_chamber("upper", "118")
+            # for event in self.scrape_senate():
+            #     if event.dedupe_key in events:
+            #         self.warning(f"Duplicate event {event.dedupe_key}")
+            #         continue
+            #     events.add(event.dedupe_key)
+            #     event_count += 1
+            #     yield event
+        if self.event_count < 1:
             raise EmptyScrape
+
+    def add_api_key(self, url):
+        req = PreparedRequest()
+        params = {"api_key": self.api_key}
+        req.prepare_url(url, params)
+        return req.url
+
+    # convert 118 -> 118th, 121 -> 121st, etc.
+    def ordinal(self, n):
+        n = int(n)
+        if 11 <= (n % 100) <= 13:
+            suffix = "th"
+        else:
+            suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+        return str(n) + suffix
+
+    def scrape_chamber(self, chamber, session, list_url=None):
+        if list_url is None:
+            list_url = f"https://api.congress.gov/v3/committee-meeting/{session}/{self.chambers[chamber]}"
+
+        list_url = self.add_api_key(list_url)
+        page = json.loads(self.get(list_url).content)
+        rows = page["committeeMeetings"]
+        for row in rows:
+            yield from self.scrape_event(row["url"])
+        if "pagination" in page and "next" in page["pagination"]:
+            yield from self.scrape_chamber(chamber, session, page["pagination"]["next"])
+
+    def scrape_event(self, url):
+        try:
+            page = json.loads(requests.get(self.add_api_key(url)).content)
+        except requests.exceptions.RequestException as e:
+            self.warning(f"Source error on {url}, skipping.")
+            self.warning(e)
+            return
+
+        if "error" in page:
+            self.warning(f"Source error on {url}, skipping.")
+            self.warning(f"Error: {page['error']}")
+            return
+
+        page = page["committeeMeeting"]
+        pprint.pprint(page)
+
+        event_date = dateutil.parser.parse(page["date"])
+
+        loc = page["location"]
+        if "address" in loc:
+            # address appears to be double encoded
+            loc = json.loads(loc["address"])
+            pprint.pprint(loc)
+            address = f"{loc['building_name']}, {loc['street-address']}, {loc['city']}, {loc['state']}, {loc['postal_code']}"
+        else:
+            building_code = loc["building"]
+            room = loc["room"]
+            if self.buildings.get(building_code):
+                address = f"{self.buildings.get(building_code)}, Room {room}"
+            else:
+                address = f"{building_code}, Room {room}"
+
+        status = "tentative"
+
+        if status == "tentative" and event_date > datetime.datetime.now(pytz.utc):
+            status = "passed"
+
+        if page["meetingStatus"] == "Cancelled":
+            status = "cancelled"
+
+        event = Event(
+            start_date=event_date,
+            name=page["title"][:1000],
+            location_name=address,
+            classification="committee-meeting",
+            upstream_id=page["eventId"],
+            status=status,
+        )
+
+        for com in page["committees"]:
+            event.add_committee(com["name"], id=com["systemCode"])
+
+        if "witnesses" in page:
+            for person in page["witnesses"]:
+                fullname = person["name"].replace("The Honorable", "").strip()
+                if person["position"].lower() != "member of congress":
+                    fullname = f"{person['name']}, {person['position']}"
+                if "organization" in person:
+                    fullname = f"{fullname} - {person['organization']}"
+                event.add_person(fullname)
+
+        if "witnessDocuments" in page:
+            for index, doc in enumerate(page["witnessDocuments"]):
+                # lots of unique docs w/ the same name,
+                # so number them so os-core doesn't combine them
+                # in principle the json might not keep order between scrapes,
+                # but it seems consistent
+                doc_name = f"{doc['documentType']} ({index})"
+                event.add_document(
+                    doc_name, doc["url"], media_type=get_media_type(doc["url"])
+                )
+
+        if "meetingDocuments" in page:
+            for index, doc in enumerate(page["meetingDocuments"]):
+
+                if "name" in doc:
+                    doc_name = doc["name"]
+                else:
+                    doc_name = f"{doc['documentType']} ({index})"
+
+                if doc["documentType"] == "Hearing: Transcript":
+                    doc_name = f"Transcript: {doc_name}"
+
+                if doc["documentType"] == "Bills and Resolutions":
+                    event.add_document(
+                        doc_name, doc["url"], media_type=get_media_type(doc["url"])
+                    )
+
+        if "relatedItems" in page and "bills" in page["relatedItems"]:
+            for bill in page["relatedItems"]["bills"]:
+                if bill["type"] == "PN":
+                    self.info(f"Skipping nomination {bill['type']} {bill['number']}")
+                    continue
+                event.add_bill(f"{bill['congress']} - {bill['type']} {bill['number']}")
+
+        if "videos" in page:
+            for vid in page["videos"]:
+                event.add_media_link(
+                    vid["name"],
+                    vid["url"],
+                    get_media_type(vid["url"], default="text/html"),
+                )
+
+        ordinal_session = self.ordinal(page["congress"])
+        ch = page["chamber"].lower()
+        event.add_source(
+            f"https://www.congress.gov/event/{ordinal_session}-congress/{ch}-event/{page['eventId']}"
+        )
+        self.geocode(event)
+
+        self.event_count += 1
+        yield event
 
     def scrape_senate(self):
         url = "https://www.senate.gov/general/committee_schedules/hearings.xml"
