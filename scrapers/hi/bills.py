3c9dd3235bc5ee96976f3ae4270b8dfb6214da1b
Tim Showers <showerst@gmail.com>
2024-01-23 15:38:42 -0500

    HI: progess on using xml

--- scrapers/hi/bills.py
+++ scrapers/hi/bills.py
@@ -5,6 +5,7 @@ from openstates.scrape import Scraper, Bill, VoteEvent
 from .actions import Categorizer, find_committee
 from .utils import get_short_codes
 from urllib import parse as urlparse
+import dateutil
 import os
 import pytz
 from zenrows import ZenRowsClient
@@ -55,9 +56,12 @@ def split_specific_votes(voters):
 
 class HIBillScraper(Scraper):
     categorizer = Categorizer()
+    bill_types = ["HB", "HR", "HCR", "SB", "SR", "SCR", "GM"]
 
     scraper = None
     request_params = {"premium_proxy": "true", "proxy_country": "us"}
+
+    tz = pytz.timezone("US/Hawaii")
 
     def parse_bill_metainf_table(self, metainf_table):
         def _sponsor_interceptor(line):
@@ -417,20 +421,45 @@ class HIBillScraper(Scraper):
             yield from self.scrape_bill(session, chamber, billtype_map, bill_url)
 
     def scrape(self, chamber=None, session=None):
-        tz = pytz.timezone("US/Hawaii")
+        
         self.scraper = ZenRowsClient(os.environ.get("ZENROWS_API_KEY"))
         get_short_codes(self, self.scraper)
         bill_types = ["bill", "cr", "r"]
         chambers = [chamber] if chamber else ["lower", "upper"]
-        day = dt.datetime.now(tz).strftime("%-m/%-d/%Y")
+        day = dt.datetime.now(self.tz).date()
+        yield from self.scrape_xml(session, day)
+
         # TODO: Turn this into an option somehow
-        yield from self.scrape_daily(session, day)
+        # day = dt.datetime.now(self.tz).strftime("%-m/%-d/%Y")
+        # yield from self.scrape_daily(session, day)
         # for chamber in chambers:
         #     # only scrape GMs once
         #     if chamber == "upper":
         #         bill_types.append("gm")
         #     for typ in bill_types:
         #         yield from self.scrape_type(chamber, session, typ)
+
+    def scrape_xml(self, session, day):
+        url = "https://www.capitol.hawaii.gov/sessions/session2024/rss/"
+        self.info(f"fetching url {url}")
+        page = self.scraper.get(url, params=self.request_params, verify=False).text
+        # this content isn't amenable to lxml, but it's machine generated so regex should be ok
+        bill_re = r"(?P<date>\d+\/\d+\/\d+)\s+(?P<time>.*?)\s+\d+\s\<a href=\"(?P<url>.*?)\">(?P<filename>.*?)\.xml<\/a>"
+        for match in re.finditer(bill_re, page, flags=re.IGNORECASE):
+            posted = dateutil.parser.parse(f"{match.group('date')} {match.group('time')}")
+            posted = self.tz.localize(posted)
+            posted = posted.date()
+            bill_type, bill_num = self.parse_bill_number(match.group('filename'))
+            if posted >= day and bill_type in self.bill_types:
+                self.warning(f"Scraping {bill_type}{bill_num} posted on {posted.strftime('%Y-%m-%d')}")
+                chamber, classification = self.classify_bill_type(match.group('filename'))
+
+                # https://www.capitol.hawaii.gov/session/measure_indiv.aspx?billtype=SB&billnumber=3013
+                bill_url = f"https://www.capitol.hawaii.gov/session/measure_indiv.aspx?billtype={bill_type}&billnumber={bill_num}"
+
+                yield from self.scrape_bill(session, chamber, classification, bill_url)
+            else:
+                self.warning(f"Skipping {bill_type}{bill_num} posted on {posted.strftime('%Y-%m-%d')}")
 
     def scrape_daily(self, session, day):
         url = f"https://www.capitol.hawaii.gov/reports/reportDailyDocs.aspx?date={day}&bills=true&resos=True&other=true"
@@ -477,3 +506,7 @@ class HIBillScraper(Scraper):
                 return val
 
         self.error(f"Invalid bill type: {bill}")
+
+    def parse_bill_number(self, bill: str) -> tuple:
+        match = re.search(r"(?P<type>[A-Z]+)(?P<number>\d+)", bill)
+        return (match.group("type"), match.group("number"))
