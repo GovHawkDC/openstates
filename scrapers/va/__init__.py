d82ac7ceb248acad2f150ce32fd368e5a5436bba
Tim Showers <showerst@gmail.com>
2024-08-12 16:05:13 -0400

    VA: reenable regular

--- scrapers/va/__init__.py
+++ scrapers/va/__init__.py
@@ -214,7 +214,7 @@ class Virginia(State):
             "name": "2024 Regular Session",
             "start_date": "2024-01-10",
             "end_date": "2024-03-09",
-            "active": False,
+            "active": True,
         },
         {
             "_scraped_name": "2024 Special Session I",
