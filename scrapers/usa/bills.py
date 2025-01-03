88e5ba4a02c6a322896eed792d109292af04dbe1
Tim Showers <showerst@gmail.com>
2025-01-03 11:10:04 -0500

    USA: debug

--- scrapers/usa/bills.py
+++ scrapers/usa/bills.py
@@ -629,12 +629,11 @@ class USBillScraper(Scraper):
                 return
 
     def scrape_senate_votes(self, bill, page, url):
-        print(f"scrape_senate_votes {url}")
-        vote_date = page.xpath("//roll_call_vote/vote_date/text()")[0].strip()
-
-        if not vote_date or vote_date == "":
+        if not page.xpath("//roll_call_vote/vote_date/text()"):
             self.error(f"Unable to parse vote date in {url}")
             return
+
+        vote_date = page.xpath("//roll_call_vote/vote_date/text()")[0].strip()
 
         when = self._TZ.localize(
             datetime.datetime.strptime(vote_date, "%B %d, %Y, %H:%M %p")
