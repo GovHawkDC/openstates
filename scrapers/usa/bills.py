0bebb93c56328a6fb2d602d6500f88d08b42227c
Tim Showers <showerst@gmail.com>
2025-06-23 09:04:13 -0400

    USA: Upstream

--- scrapers/usa/bills.py
+++ scrapers/usa/bills.py
@@ -176,21 +176,28 @@ class USBillScraper(Scraper):
             classification=classification,
         )
 
-        self.scrape_actions(bill, xml)
-        self.scrape_amendments(bill, xml, session, chamber, bill_id)
-        self.scrape_cbo(bill, xml)
-        self.scrape_committee_reports(bill, xml)
-        self.scrape_cosponsors(bill, xml)
-        self.scrape_laws(bill, xml)
-        self.scrape_related_bills(bill, xml)
-        self.scrape_sponsors(bill, xml)
-        self.scrape_subjects(bill, xml)
-        self.scrape_summaries(bill, xml)
-        self.scrape_titles(bill, xml)
-        self.scrape_versions(bill, xml)
+        try:
+            self.scrape_actions(bill, xml)
+            self.scrape_amendments(bill, xml, session, chamber, bill_id)
+            self.scrape_cbo(bill, xml)
+            self.scrape_committee_reports(bill, xml)
+            self.scrape_cosponsors(bill, xml)
+            self.scrape_laws(bill, xml)
+            self.scrape_related_bills(bill, xml)
+            self.scrape_sponsors(bill, xml)
+            self.scrape_subjects(bill, xml)
+            self.scrape_summaries(bill, xml)
+            self.scrape_titles(bill, xml)
+            self.scrape_versions(bill, xml)
 
-        for vote in self.scrape_votes(bill, xml):
-            yield vote
+            for vote in self.scrape_votes(bill, xml):
+                yield vote
+        except Exception as e:
+            self.warning(f"Error parsing bill {bill_id}: {e}")
+            self.error(
+                f"XML content: {ET.tostring(xml, encoding='unicode', method='xml')}"
+            )
+            raise e
 
         xml_url = f"https://www.govinfo.gov/bulkdata/BILLSTATUS/{session}/{bill_type.lower()}/BILLSTATUS-{session}{bill_type.lower()}{bill_num}.xml"
         bill.add_source(xml_url)
@@ -624,7 +631,7 @@ class USBillScraper(Scraper):
         for row in xml.findall("bill/actions/item/recordedVotes/recordedVote"):
             url = self.get_xpath(row, "url")
             chamber = self.get_xpath(row, "chamber")
-            if url not in vote_urls:
+            if (url, chamber) not in vote_urls:
                 vote_urls.append((url, chamber))
 
         for url, chamber in vote_urls:
@@ -642,6 +649,12 @@ class USBillScraper(Scraper):
             except (requests.exceptions.HTTPError, lxml.etree.XMLSyntaxError):
                 self.info(f"Error fetching {url}, skipping (used requests, no retries)")
                 return
+            except Exception as e:
+                self.warning(f"Error parsing vote at {url}: {e}")
+                self.error(
+                    f"XML content: {ET.tostring(vote_xml, encoding='unicode', method='xml')}"
+                )
+                raise e
 
     def scrape_senate_votes(self, page, url):
         if not page.xpath("//roll_call_vote/vote_date/text()"):
