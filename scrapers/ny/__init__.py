0744ff059213324fc72241390901a146c1e6a40b
Tim Showers <showerst@gmail.com>
2025-07-10 08:20:13 -0400

    NY: upstream changes

--- scrapers/ny/__init__.py
+++ scrapers/ny/__init__.py
@@ -1,9 +1,7 @@
+from utils import url_xpath
 from openstates.scrape import State
 from .bills import NYBillScraper
 from .events import NYEventScraper
-
-import lxml
-import requests
 
 
 settings = dict(SCRAPELIB_TIMEOUT=120)
@@ -85,20 +83,7 @@ class NewYork(State):
     ignored_scraped_sessions = []
 
     def get_session_list(self):
-        # return self.get(
-        #     "https://nysenate.gov/search/legislation",
-        #     '',
-        #     verify=False
-        # )
-    
-        ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-        headers = {"User-Agent": ua}
-
-        url = "https://nysenate.gov/search/legislation"
-
-        page = requests.get(url, headers=headers, verify=False).content
-        page = lxml.html.fromstring(page)
-
-        sessions = page.xpath('//select[@name="session_year"]/option[@value!="0"]/@value')
-        return sessions
-
+        return url_xpath(
+            "http://nysenate.gov/search/legislation",
+            '//select[@name="session_year"]/option[@value!="0"]/@value',
+        )
